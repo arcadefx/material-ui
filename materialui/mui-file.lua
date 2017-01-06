dead0076ed1195253f27dbbbb557f2d5b7446168
arcadefx <pcardwell@gannett.com>
2017-01-06 18:59:22 -0500

    added useTimeOut and timeOut params to newToast() to auto close notification

---
+++ materialui/mui-file.lua
@@ -0,0 +1,43 @@
+--[[
+    A loosely based Material UI module
+
+    mui-file.lua : The file io module.
+
+    The MIT License (MIT)
+
+    Copyright (C) 2016 Anedix Technologies, Inc.  All Rights Reserved.
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    For other software and binaries included in this module see their licenses.
+    The license and the software must remain in full when copying or distributing.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+
+--]]
+
+local muiData = require( "materialui.mui-data" )
+local M = muiData.M -- {} -- for module array/table
+
+function file_exists(name)
+   local fileHandle, errorMessage = io.open(name,"r")
+   if M.errorMessage == nil then errorMessage = "" end
+   M.errorMessage = M.errorMessage + errorMessage
+   if fileHandle ~= nil then io.close(fileHandle) return true else return false end
+end
+
+return M
